0549c4893f88b1f3bdb14ca304175316ce8a0a23
Rhys Evans <rhys.r.evans@stfc.ac.uk>
2020-10-01 13:15:01 +0000

    Keycloak sign out fix.

--- ceda_account_site/urls.py
+++ ceda_account_site/urls.py
@@ -9,10 +9,13 @@ __license__ = "BSD - see LICENSE file in top-level directory"
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic.base import RedirectView
+from . import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('logout', views.keycloak_logout, name='logout'),
+    path('account', views.keycloak_account, name='account'),
     path('oidc/', include('mozilla_django_oidc.urls')),
     path('services/', include('jasmin_services.urls', namespace = 'services')),
     path('notifications/', include('jasmin_notifications.urls', namespace = 'notifications')),
