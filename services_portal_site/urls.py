3c18176ec7c0ca7f0ac8431aa04c03c427497323
watucker <william.tucker@stfc.ac.uk>
2021-02-02 17:23:03 +0000

    Added health check endpoint

--- services_portal_site/urls.py
+++ services_portal_site/urls.py
@@ -7,10 +7,16 @@ __license__ = "BSD - see LICENSE file in top-level directory"
 
 
 from django.contrib import admin
+from django.shortcuts import HttpResponse
 from django.urls import include, path
 
 
+def health_view(request):
+    return HttpResponse("OK")
+
+
 urlpatterns = [
+    path("health/", health_view, name="health"),
     path("admin/", admin.site.urls),
     path("oidc/", include("mozilla_django_oidc.urls")),
     path("services/", include("jasmin_services.urls", namespace="services")),
