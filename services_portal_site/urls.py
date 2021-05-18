fa2a25d72be8a387fd0f39004dd04728b978c283
rhysrevans3 <rhys.r.evans3@gmail.com>
2021-05-18 10:27:24 +0100

    Adding redirect for root url.

--- services_portal_site/urls.py
+++ services_portal_site/urls.py
@@ -8,7 +8,7 @@ __license__ = "BSD - see LICENSE file in top-level directory"
 
 from django.contrib import admin
 from django.shortcuts import HttpResponse
-from django.urls import include, path
+from django.views.generic.base import RedirectView
 
 
 def health_view(request):
@@ -16,7 +16,7 @@ def health_view(request):
 
 
 urlpatterns = [
-    path("health/", health_view, name="health"),
+    path('', RedirectView.as_view(url = 'services'), name = 'portal_root'),path("health/", health_view, name="health"),
     path("admin/", admin.site.urls),
     path("oidc/", include("mozilla_django_oidc.urls")),
     path("services/", include("jasmin_services.urls", namespace="services")),
